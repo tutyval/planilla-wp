b202230d82d150663ca2549af67bd71c25f39068
Susana Valverde <svalverdeh@gmail.com>
2018-08-17 21:18:56 -0300

    primeros codigos

---
+++ wordpress/wp-content/themes/tema-2/functions.php
@@ -0,0 +1,6 @@
+<?php
+wp_register_style( $handle, $src, $deps, $ver,$media );
+?>
+<?php
+wp_register_script( $handle, $src, $deps,$ver, $in_footer );
+?>
--- wordpress/wp-content/themes/tema-2/header.php
+++ wordpress/wp-content/themes/tema-2/header.php
@@ -1,1 +1,11 @@
-header.php
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+<meta charset="<?php bloginfo('charset') ?>">
+<meta name="viewport" content="width=device-width, initialscale=
+1">
+<title><?php bloginfo('name'); ?></title>
+<?php wp_head() ?>
+</head>
+<body>
+	
